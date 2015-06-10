c8a30aa41bf364fc128d6fd711be5c05a48a6862
NikitaNikishkin292 <nikita.nikishkin@gmail.com>
2015-06-10 16:16:13 +0300

    settings.py for staticfiles2.0

---
+++ recycle/recycle/static/control_measure/script.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+	//обработка нажатия на кнопку "добавить контейнер"
+	$('#new_bin').click(function(event){
+		$('#new_bin_info').removeClass('hidden_menu');
+		$('#new_bin_info').addClass('visible_menu');
+	});
+
+	//обработчики кнопки "выгрузить"
+	$('.unload_button').click(function(event){
+		$('#new_unload_info').removeClass('hidden_menu');
+		$('#new_unload_info').addClass('visible_menu');
+	});
+
+	//обработчики кнопки Эдобавить измерение
+	$('.add_button').click(function(event){
+		$('#new_measure_info').removeClass('hidden_menu');
+		$('#new_measure_info').addClass('visible_menu');
+	});
+});
